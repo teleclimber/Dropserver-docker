ce962f83a5b6e877ee04374ab0c9e376d2ea8755
O F <teleclimber@gmail.com>
2024-01-25 10:31:07 -0800

    Update to ds-host 0.13

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-ds_host_ver="0.12.0"
-deno_ver="1.37.2"
+ds_host_ver="0.13.0"
+deno_ver="1.39.4"
 base_img="ubuntu:22.10"
 ds_host_bin_repo="teleclimber/ds-host-bin"	# Note: change ref to this image in Docker file too
 ds_host_repo="teleclimber/ds-host"
