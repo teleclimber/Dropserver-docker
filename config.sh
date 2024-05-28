289cd5f429305d118e3b3b26593b2ef3b5801001
O F <teleclimber@gmail.com>
2024-05-28 12:40:21 -0700

    Update to 0.13.2 and latest deno

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-ds_host_ver="0.13.1"
-deno_ver="1.41.2"
+ds_host_ver="0.13.2"
+deno_ver="1.43.6"
 base_img="ubuntu:22.10"
 ds_host_bin_repo="teleclimber/ds-host-bin"	# Note: change ref to this image in Docker file too
 ds_host_repo="teleclimber/ds-host"
